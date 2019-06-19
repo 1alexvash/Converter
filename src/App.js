6b80d876b0ea8798479586d5f9583ca0857dbd9d
Alexander <ilordarkey@gmail.com>
2019-06-19 10:55:16 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,132 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./scss/main.css";
 
-function App() {
+import Data from "./Data";
+
+const App = () => {
+  const [state, setState] = useState({
+    tab: Object.keys(Data)[0],
+    unit1: null,
+    unit2: null,
+    input: 0,
+    output: 0
+  });
+
+  useEffect(() => {
+    setState({
+      ...state,
+      unit1: Data[state.tab][0].name,
+      unit2: Data[state.tab][1].name
+    });
+    // eslint-disable-next-line
+  }, [state.tab]);
+
+  useEffect(() => {
+    if (typeof state.unit1 === "string" && typeof state.unit2 === "string") {
+      updateState(state.input);
+    }
+    // eslint-disable-next-line
+  }, [state.unit1, state.unit2]);
+
+  function updateState(value) {
+    const val1 =
+      Data[state.tab].find(measure => measure.name === state.unit1).value *
+      1000;
+    const val2 =
+      Data[state.tab].find(measure => measure.name === state.unit2).value *
+      1000;
+
+    setState({
+      ...state,
+      input: value,
+      output: (value * val1) / val2
+    });
+  }
+
+  function swap() {
+    setState({
+      ...state,
+      unit1: state.unit2,
+      unit2: state.unit1
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Units">
+        {Object.keys(Data).map(measure => (
+          <button
+            key={measure}
+            onClick={e => setState({ ...state, tab: e.target.innerHTML })}
+            className={state.tab === measure ? "active" : ""}
+          >
+            {measure}
+          </button>
+        ))}
+      </div>
+      <div className="List-of-units">
+        <section>
+          {Data[state.tab].map(measure => (
+            <button
+              key={measure.name}
+              onClick={e =>
+                setState({
+                  ...state,
+                  unit1: measure.name
+                })
+              }
+              className={state.unit1 === measure.name ? "active" : ""}
+            >
+              {measure.name}
+            </button>
+          ))}
+        </section>
+        <section>
+          {Data[state.tab].map(measure => (
+            <button
+              key={measure.name}
+              onClick={e =>
+                setState({
+                  ...state,
+                  unit2: measure.name
+                })
+              }
+              className={state.unit2 === measure.name ? "active" : ""}
+            >
+              {measure.name}
+            </button>
+          ))}
+        </section>
+        <img
+          src="swap.png"
+          onClick={() => swap()}
+          alt="swap"
+          className="swap"
+        />
+      </div>
+      <div className="Converter">
+        <label>
+          <p>{state.unit1}</p>
+          <input
+            type="number"
+            className="input"
+            value={state.input}
+            onChange={e => updateState(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <p>{state.unit2}</p>
+          <input
+            type="number"
+            className="output"
+            disabled
+            value={state.output}
+          />
+        </label>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Data.js
@@ -0,0 +1,30 @@
+export default {
+  Distances: [
+    { name: "Kilometre", value: 1000 },
+    { name: "Metre", value: 1 },
+    { name: "Centimetre", value: 0.01 },
+    { name: "Millimetre", value: 0.001 },
+    { name: "Mile", value: 1610 },
+    { name: "Yard", value: 0.9 },
+    { name: "Foot", value: 0.3 },
+    { name: "Inch", value: 0.025 }
+  ],
+  Weights: [
+    { name: "Kilogram", value: 1 },
+    { name: "Gram", value: 0.001 },
+    { name: "Tonne", value: 1000 },
+    { name: "Milligram", value: 0.000001 },
+    { name: "Stone", value: 0.157 },
+    { name: "Pound", value: 2.2 },
+    { name: "Ounce", value: 35 }
+  ],
+  Time: [
+    { name: "Second", value: 1 },
+    { name: "Minute", value: 60 },
+    { name: "Hour", value: 3600 },
+    { name: "Day", value: 86400 },
+    { name: "Week", value: 604800 },
+    { name: "Month", value: 2628000 },
+    { name: "Year", value: 31540000 }
+  ]
+};
